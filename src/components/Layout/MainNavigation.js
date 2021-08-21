2b04c180b4b3f351f66b27e926d7f07fdcbc109f
ayushi <ayushi3007dwivedi@gmail.com>
2021-08-22 01:09:09 +0530

    commit of src public and package.json

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,15 +1,17 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
-
+import { useHistory } from 'react-router';
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+  const history = useHistory();
   const authCtx=useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
 
   const logoutHandler = ()=>{
     authCtx.logout();
+    history.replace('/');
   };
 
   return (
@@ -24,7 +26,7 @@ const MainNavigation = () => {
           </li>)}
           
           {isLoggedIn && (<li>
-            <Link to='/upload'>Upload</Link>
+            <Link to='/upload'>Upload and Display</Link>
           </li>)}
 
           {isLoggedIn && (<li>
--- src/components/Upload/FileUpload.js
+++ src/components/Upload/FileUpload.js
@@ -1,25 +1,73 @@
 import classes from './FileUpload.module.css';
-import {useState} from 'react';
-import storage from '../../pages/firebase';
+import React, { useState } from 'react';
+import doAjax from '../Helper/doAjax';
+
 const FileUpload = () => {
-const [filetoUpload , setFile] = useState('');
-const upload = ()=>{
-  if(filetoUpload == null)
-    return;
-  storage.ref(`${filetoUpload.name}`).put(filetoUpload)
-  .on("state_changed" , alert("success") , alert);
-}
-  
+  const [filetoUpload, setFiles] = useState('');
+  const [displaydata, setDisplaydata] = useState(null);
+  const upload = (e) => {
+    e.preventDefault();
+    if (filetoUpload == null)
+      return;
+    doAjax.post("/logitech.json", filetoUpload);
+  }
+  const handleChange = e => {
+    e.preventDefault();
+    const fileReader = new FileReader();
+    fileReader.readAsText(e.target.files[0], "UTF-8");
+    fileReader.onload = e => {
+      console.log("e.target.result", e.target.result);
+      setFiles(e.target.result);
+      doAjax.post("/logitech.json", filetoUpload);
+    };
+  };
+  const getJsonFileData = e => {
+    e.preventDefault();
+    doAjax.get('/logitech.json')
+      .then((res) => {
+        console.log(Object.values(res.data));
+        if(Object.values(res.data)!==null){
+          var mydata = Object.values(res.data);
+          setDisplaydata(mydata);
+        }
+        
+      });
+  };
   return (
-     <form className={classes.form}>
-     <div className={classes.control}>
-       <label >Upload File</label>
-       <input type="file" onChange={(e)=>{setFile(e.target.files[0])}} />
-     </div>
-     <div className={classes.action}>
-       <button onClick={upload}>Upload</button>
-     </div>
-   </form>
+    <React.Fragment>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label >Upload File</label>
+          <input type="file" onChange={handleChange} />
+        </div>
+        <div className={classes.action}>
+          <button onClick={upload}>Upload</button>
+        </div>
+        <div className={classes.action}>
+          <button onClick={getJsonFileData}>display</button>
+        </div>
+        
+      </form>
+      
+      {displaydata !== null && <div><table >
+        <tr>
+          <th>UserID</th>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+        
+        {displaydata[0].map((item) => (
+        <tr>
+          <td>{item.userId}</td>
+          <td>{item.id}</td>
+          <td>{item.title}</td>
+          <td>{item.body}</td>
+        </tr>
+       ))}
+      </table>
+      </div>}
+    </React.Fragment>
   );
 };
 
